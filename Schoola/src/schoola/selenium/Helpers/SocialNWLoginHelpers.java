0c2d75ea09be4849b6aa1f8c6e68126192266d46
unknown <asmita_gaonker@.persistent.co.in>
2014-10-15 14:23:18 +0530

    ShareOnFacebook function added

--- Schoola/src/schoola/selenium/Helpers/SocialNWLoginHelpers.java
+++ Schoola/src/schoola/selenium/Helpers/SocialNWLoginHelpers.java
@@ -1,12 +1,25 @@
 package schoola.selenium.Helpers;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SocialNWLoginHelpers {
 	
-	public void gotoFacebookLogin(WebDriver driver){
-		driver.findElement(By.cssSelector("div.share-and-activity div.s-sharing-box div.social-block div a.btn btn-facebook")).click();
-	}		
+	public void goToShareOnFacebook_button(WebDriver driver){
+		driver.findElement(By.cssSelector("div#s-body div.w div.w_r div.share-and-activity div.s-sharing-box div.social-block div a.btn")).click();
+	}
+	
+	public void Login_ShareOnFacebook(WebDriver driver){
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.findElement(By.id("email")).sendKeys("agsavy1@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("testuser123");
+		driver.findElement(By.id("u_0_1")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.id("u_0_2")).click();		
+	}
+	
+	
 	
 }
